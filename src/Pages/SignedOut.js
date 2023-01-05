0368f5d260234d4e619b76a4770fb9321e9cce17
iamrch <roman.hinds17@gmail.com>
2023-01-04 23:14:42 -0500

    updated sign in page

--- src/Pages/SignedOut.js
+++ src/Pages/SignedOut.js
@@ -3,9 +3,11 @@ import React from 'react';
 function SignedOut({signInWithGoogle}) {
   
   return (
-    <div>
-        <h1>You are signed out! Sign in below:</h1>
-        <button onClick={() => signInWithGoogle()}>Sign In</button>
+    <div className='SignedOut'>
+        <img className='SplashLogo' src="/StessBoardLogo.png" alt="StressBoard Logo"/>
+        <h1 className='SplashHeader'>Welcome to StressBoard by HERO</h1>
+        <h3 className='SplashText'>Please sign in below:</h3>
+        <button className='SignInButton' onClick={() => signInWithGoogle()}>Sign In</button>
     </div>
   )
 }
